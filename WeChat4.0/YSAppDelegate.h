20f2e88ca7c96019fcfb0cc9094379b3f72d5a57
YS_RMBP <mac@abcdedeMacBook-Pro.local>
2014-05-26 09:21:49 +0800

    Initial Commit

---
+++ WeChat4.0/YSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YSAppDelegate.h
+//  WeChat4.0
+//
+//  Created by YS_RMBP on 14-5-26.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
